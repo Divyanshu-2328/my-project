2930458f0e90f70762c8f9edc10e5699fdfc52f8
Divyanshu-2328 <shashibalasingh215325@gmail.com>
2025-09-19 11:58:18 +0530

    REact Project

---
+++ React-Project/my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductCart from './productCart.jsx'
+
+function App() {
+ 
+
+  return (
+    <div>
+        <div>Product Cart</div>
+        <div>Product Price</div>
+        <div>Product Stock</div>
+    </div>
+    
+  )
+  
+}
+
+export default App
